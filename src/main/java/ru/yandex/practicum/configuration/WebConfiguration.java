86ddc245d2d8e1b6ad6a9fb9325b085701a3ea65
Synner Lee <sinner.li>
2025-10-25 20:34:51 +0500

    Added CORS for http://localhost

--- src/main/java/ru/yandex/practicum/configuration/WebConfiguration.java
+++ src/main/java/ru/yandex/practicum/configuration/WebConfiguration.java
@@ -3,10 +3,21 @@ package ru.yandex.practicum.configuration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "ru.yandex.practicum")
-public class WebConfiguration {
+@PropertySource("classpath:application.properties")
+public class WebConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost", "http://localhost:80", "http://127.0.0.1:80")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
 }
